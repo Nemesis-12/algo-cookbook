ab1cebe733521fe216d86832a212deac5264b62e
NemesisTCO <56759448+Nemesis-12@users.noreply.github.com>
2023-04-11 08:06:58 -0500

    Update insertion_sort.py

--- Python/insertion_sort.py
+++ Python/insertion_sort.py
@@ -1,1 +1,13 @@
+# Program to implement insertion sort algorithm
 
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        j = i
+        while j > 0 and arr[j] < arr[j-1]:
+            arr[j], arr[j-1] = arr[j-1], arr[j]
+            j -= 1
+    return arr
+
+if __name__ == '__main__':
+    arr = [1, 5, 2, 4, 3]
+    print(insertion_sort(arr))
